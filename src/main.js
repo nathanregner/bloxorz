a7f19d654a40ec79dd31996e25be0f653e401172
Nathan Regner <nathanregner@gmail.com>
2019-11-29 01:28:28 -0700

    Add block and tile textures

--- src/main.js
+++ src/main.js
@@ -7,23 +7,40 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
+camera.position.z = 5;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+function createBlock() {
+  const texture = new THREE.TextureLoader().load('assets/block.png');
+  return new THREE.Mesh(
+    new THREE.BoxGeometry(1, 2, 1),
+    new THREE.MeshBasicMaterial({ map: texture })
+  );
+}
 
-camera.position.z = 5;
+const block = createBlock();
+scene.add(block);
+
+function createTile() {
+  const texture = new THREE.TextureLoader().load('assets/tile.png');
+  return new THREE.Mesh(
+    new THREE.BoxGeometry(1, 0.3, 1),
+    new THREE.MeshBasicMaterial({ map: texture })
+  );
+}
+
+const tile = createTile();
+tile.position.y = -1;
+scene.add(tile);
 
 const animate = function() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  block.rotation.x += 0.01;
+  block.rotation.y += 0.01;
 
   renderer.render(scene, camera);
 };
