c06e91f49c87b04e9e6f27302c0ff34b2ddbab67
Nathan Regner <nathanregner@gmail.com>
2019-12-16 19:42:10 -0700

    Cleanup main

--- src/main.ts
+++ src/main.ts
@@ -3,77 +3,88 @@ import { Directions } from './entities/block';
 import { Game } from './game';
 
 const scene = new THREE.Scene();
-
-const camera = new THREE.PerspectiveCamera(75, undefined, 0.1, 1000);
-camera.position.y = 5;
-
 const renderer = new THREE.WebGLRenderer({ alpha: true });
-
-function resize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-resize();
-window.addEventListener('resize', resize);
-
 document.body.appendChild(renderer.domElement);
 
-function setLevelHash(level: number) {
-  window.location.hash = String(level);
-}
+const camera = new THREE.PerspectiveCamera(75, undefined, 0.1, 1000);
+camera.position.y = 5.5;
 
-function getLevelHash() {
-  return parseInt(window.location.hash.substring(1));
-}
-
-function autoFitCamera(obj: THREE.Object3D, camera: THREE.PerspectiveCamera) {
-  const boundingSphere = new THREE.Box3()
-    .setFromObject(obj)
-    .getBoundingSphere(new THREE.Sphere());
-
-  camera.position.setX(boundingSphere.center.x);
-  camera.position.setZ(boundingSphere.radius * 1.1);
-  camera.lookAt(boundingSphere.center);
-
-  camera.updateProjectionMatrix();
-}
-
-const game = new Game(scene, {
-  onLevelSwitched: levelNumber => {
-    setLevelHash(levelNumber);
-    autoFitCamera(game.getLevelObj3d(), camera);
-  },
-});
-
-game.loadLevel(getLevelHash() || 0);
-window.onhashchange = () => game.loadLevel(getLevelHash());
-
-const moveKeys = {
-  a: Directions.W,
-  d: Directions.E,
-  s: Directions.S,
-  w: Directions.N,
-};
-
-document.addEventListener('keypress', ev => {
-  const direction = moveKeys[ev.key];
-  if (direction) {
-    game.moveBlock(direction);
-  } else {
-    switch (ev.key) {
-      case 'r':
-        game.restartLevel();
-        break;
-    }
+function createGame() {
+  function setLevelHash(level: number) {
+    window.location.hash = String(level);
   }
-});
 
-const animate = function() {
+  function getLevelHash() {
+    return parseInt(window.location.hash.substring(1));
+  }
+
+  function autoFitCamera(obj: THREE.Object3D) {
+    const boundingSphere = new THREE.Box3()
+      .setFromObject(obj)
+      .getBoundingSphere(new THREE.Sphere());
+
+    camera.position.setX(boundingSphere.center.x);
+    camera.position.setZ(boundingSphere.radius * 1.3);
+    camera.lookAt(boundingSphere.center);
+
+    camera.updateProjectionMatrix();
+  }
+
+  const game = new Game(scene, {
+    onLevelSwitched: levelNumber => {
+      setLevelHash(levelNumber);
+      autoFitCamera(game.getLevelObj3d());
+    },
+  });
+
+  game.loadLevel(getLevelHash() || 0);
+  window.onhashchange = () => game.loadLevel(getLevelHash());
+  return game;
+}
+
+function setupEventHandlers() {
+  function resize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  window.addEventListener('resize', resize);
+  resize();
+}
+
+function setupKeyBindings(game: Game) {
+  const moveKeys = {
+    a: Directions.W,
+    d: Directions.E,
+    s: Directions.S,
+    w: Directions.N,
+  };
+
+  document.addEventListener('keypress', ev => {
+    const direction = moveKeys[ev.key];
+    if (direction) {
+      game.moveBlock(direction);
+    } else {
+      switch (ev.key) {
+        case 'r':
+          game.restartLevel();
+          break;
+      }
+    }
+  });
+}
+
+(function() {
+  // setup game state
+  const game = createGame();
+  setupKeyBindings(game);
+  setupEventHandlers();
+
+  // start render loop
+  function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+  }
   requestAnimationFrame(animate);
-
-  renderer.render(scene, camera);
-};
-
-requestAnimationFrame(animate);
+})();
